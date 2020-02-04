3b5bbf4c90d3218517a3f3de0f3f8c80a91b8735
Warren Brown <warrengbrn@gmail.com>
2020-02-04 02:22:08 -0500

    Allows users to enter a series of prompts that allow for pushing worker roles, location, and contact information to html file, ending when the user decides they are finished. Cards flow across page responsively. Individual credentials for different roles are simplified into less functions.

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 var createHTML = require("create-html");
+const cheerio = require('cheerio');
 const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern")
+const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 var id = 0;
 
-function askManager(){
+
+
+managerPrompt();
+
+//first prompt that creates the html file and makes manager card
+function managerPrompt(){
    
-    id++;
+  
 
 inquirer.prompt([
     {
         type: "input",
-        name: "managerName",
+        name: "name",
         message: "What is the name of the manager of this team?"
     },
     {
         type: "input",
-        name: "managerEmail",
+        name: "email",
         message: "What is the email address of this manager?"
     },
     {
         type: "input",
-        name: "officeNumber",
+        name: "special",
         message: "What is this manager's office number?"
     },
     {
         type: "list",
-        name: "employeeTypePicker",
+        name: "picktype",
         message: "Which type of worker would you like to add next?",
         choices: [
             "Engineer",
@@ -41,96 +49,38 @@ inquirer.prompt([
 
     .then(function(response){
 
-        if(response.employeeTypePicker == "Engineer"){
-            engineerPrompt();
-        }else if (response.employeeTypePicker == "Intern"){
-            internPrompt();
-        }else if (response.employeeTypePicker == "Normal Employee"){
-            normalEmployeePrompt();
-        }
-
-       const managerInfo = new Manager(response.managerName, id, response.managerEmail, response.officeNumber); 
-       var newName = managerInfo.getName();
-       var newId = managerInfo.getId();
-       var newEmail = managerInfo.getEmail();
-       var newOfficeNumber = managerInfo.getOfficeNumber();
-       var newRole = managerInfo.getRole();
-
-       var html = createHTML({
-           title: 'My Team',
-           css: ['https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css','style.css'],
-           head: '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>',
-           body: `
-                    <h1 id="header">My Team!</h1>
-                        
-           <div id="cardHolder">          
-               <div class="columns is-multiline">
-                   <div class="column is-one-third">
-                       <div class="card">
-                           <div class="card-content">
-                               <div class="titleContent">      
-                               <p id="bigtitle">${newName}</p>
-                               <p id="smalltitle">${newRole}</p>
-                                </div> 
-                                <div class="otherContent">
-                                <br>
-                               <p>ID: ${newId}</p>
-                               <br>
-                               <p>Email: ${newEmail}</p>
-                               <br>
-                               <p>Office number: ${newOfficeNumber}</p>
-                               <br>
-                                </div>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
-                    `
-       });
-
-
-
-       fs.writeFile('./templates/manager.html', html, function (err){
-           if (err) console.log(err)
-       });
+        createManagerCardNewQuestion(response);
 
     })
 
 }
 
-askManager();
 
 
-
-function thenResponse(){
-
-}
-
-
+//engineer prompt
 function engineerPrompt(){
 
-    id++;
+
 
     inquirer.prompt([
         {
             type: "input",
-            name: "managerName",
-            message: "What is the name of the manager of this team?"
+            name: "name",
+            message: "What is the name of this engineer?"
         },
         {
             type: "input",
-            name: "managerEmail",
-            message: "What is the email address of this manager?"
+            name: "email",
+            message: "What is the email address of this engineer?"
         },
         {
             type: "input",
-            name: "github",
+            name: "special",
             message: "What is this engineer's github name?"
         },
         {
             type: "list",
-            name: "employeeTypePicker",
+            name: "picktype",
             message: "Which type of worker would you like to add next?",
             choices: [
                 "Engineer",
@@ -141,30 +91,50 @@ function engineerPrompt(){
         },
         
     ])
+    .then(function(response){
+        const workerObject = new Engineer(response.name, id, response.email, response.special); 
+        var specialPrepend = 'Github username: ';
+        otherEmployees(response, workerObject, specialPrepend);
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+    
 }
+
+//internPrompt
 function internPrompt(){
 
-    id++;
+  
 
     inquirer.prompt([
         {
             type: "input",
-            name: "managerName",
-            message: "What is the name of the manager of this team?"
+            name: "name",
+            message: "What is the name of this intern?"
         },
         {
             type: "input",
-            name: "managerEmail",
-            message: "What is the email address of this manager?"
+            name: "email",
+            message: "What is the email address of this intern?"
         },
         {
             type: "input",
-            name: "github",
-            message: "What is this engineer's github name?"
+            name: "special",
+            message: "What school is this intern attending?"
         },
         {
             type: "list",
-            name: "employeeTypePicker",
+            name: "picktype",
             message: "Which type of worker would you like to add next?",
             choices: [
                 "Engineer",
@@ -175,5 +145,207 @@ function internPrompt(){
         },
         
     ])
+    .then(function(response){
+
+        const workerObject = new Intern(response.name, id, response.email, response.special); 
+        var specialPrepend = 'School: ';
+        otherEmployees(response, workerObject, specialPrepend);
+
+    })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+//normal employee prompt
+function normalEmployeePrompt(){
+
+    
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of this employee?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the email address of this employee?"
+        },
+        {
+            type: "list",
+            name: "picktype",
+            message: "Which type of worker would you like to add next?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Normal Employee",
+                "I don't want to add anymore employees, set me free!"
+            ]
+        },
+        
+    ])
+    .then(function(response){
+
+        const workerObject = new Employee(response.name, id, response.email); 
+        var specialPrepend = '';
+        otherEmployees(response, workerObject, specialPrepend);
+    })
+}
+
+
+
+
+
+
+
+//read and write the updates
+function readAndWrite(newWorker){
+
+    //reads html document
+    fs.readFile('output/team.html', "utf8", function(error, data){
+
+        if (error) {
+            return console.log(error);
+        }
+
+        //stores file data in variable
+        const $ = cheerio.load(data);
+
+        //stores html document in variable
+        const entireDocument = $('html');
+
+        //hooks into workerAdder to add a worker
+        $('#workerAdder').append(newWorker);
+
+        //writes changes to html file
+        fs.writeFile('output/team.html', entireDocument, (err) =>{
+            if (err) throw err;
+        })
+    })
+}
+
+
+
+//creates a Manager card and a new question, then updates html file
+function createManagerCardNewQuestion(response){
+
+    if(response.picktype == "Engineer"){
+        engineerPrompt();
+    }else if (response.picktype == "Intern"){
+        internPrompt();
+    }else if (response.picktype == "Normal Employee"){
+        normalEmployeePrompt();
+    }
+
+const workerObject = new Manager(response.name, id, response.email, response.special); 
+var newName = workerObject.getName();
+var newId = workerObject.getId();
+var newEmail = workerObject.getEmail();
+var newSpecial = workerObject.getSpecial();
+var newRole = workerObject.getRole();
+
+var html = createHTML({
+    title: 'My Team',
+    script: 'app.js',
+    css: ['https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css','../templates/style.css'],
+    head: '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>',
+    body: `
+             <h1 id="header">My Team!</h1>
+                 
+    <div id="cardHolder">          
+        <div class="columns is-multiline" id="workerAdder">
+            <div class="column is-one-third">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="titleContent">      
+                        <p id="bigtitle">${newName}</p>
+                        <p id="smalltitle">${newRole}</p>
+                         </div> 
+                         <div class="otherContent">
+                         <br>
+                        <p>ID: ${newId}</p>
+                        <br>
+                        <p>Email: ${newEmail}</p>
+                        <br>
+                        <p>Office number: ${newSpecial}</p>
+                        <br>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+             `
+});
+
+
+
+fs.writeFile('output/team.html', html, function (err){
+    if (err) console.log(err)
+});
+
+id++;
+}
+
+
+
+
+//creates cards for other employees
+function otherEmployees(response, workerObject, specialPrepend){
+
+if(response.picktype == "Engineer"){
+    engineerPrompt();
+}else if (response.picktype == "Intern"){
+    internPrompt();
+}else if (response.picktype == "Normal Employee"){
+    normalEmployeePrompt();
+}
+
+
+        var newName = workerObject.getName();
+        var newId = workerObject.getId();
+        var newEmail = workerObject.getEmail();
+
+        if(Object.keys(workerObject).length < 4){
+            var newSpecial = "";
+        }else{
+            var newSpecial = workerObject.getSpecial();
+        }
+        
+        var newRole = workerObject.getRole();
+
+
+
+var newWorker = `<div class="column is-one-third">
+<div class="card">
+    <div class="card-content">
+        <div class="titleContent">      
+        <p id="bigtitle">${newName}</p>
+        <p id="smalltitle">${newRole}</p>
+         </div> 
+         <div class="otherContent">
+         <br>
+        <p>ID: ${newId}</p>
+        <br>
+        <p>Email: ${newEmail}</p>
+        <br>
+        <p>${specialPrepend}${newSpecial}</p>
+        <br>
+         </div>
+    </div>
+</div>
+</div>`
+
+readAndWrite(newWorker);
+id++;
+}
